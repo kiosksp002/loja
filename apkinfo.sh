25ac7a7fccd3cff697b3147cb9d1a7262d48369f
cet <renatom@cetsp.com.br>
2024-02-27 14:44:23 -0300

    WhatsApp_240477000.apk

---
+++ apkinfo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+STRLINE=`aapt dump badging $1 | grep "^package:" | cut -d" " -f2,3`
+
+HASHCOD=`md5sum $1 | cut -d" " -f1`
+PKGNAME=`echo $STRLINE | cut -d"'" -f2`
+VERCODE=`echo $STRLINE | cut -d"'" -f4`
+FILENAM=`basename $1`
+
+
+echo "FHASH='$HASHCOD'"
+echo "PKGN='$PKGNAME'"
+echo "VC='$VERCODE'"
+echo "data['link']='https://raw.githubusercontent.com/kiosksp002/loja/main/$FILENAM'"
+
+
+
+
+
